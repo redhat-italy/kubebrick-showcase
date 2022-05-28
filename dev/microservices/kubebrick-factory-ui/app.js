564a28ac5ddaafe1df1a91a6a5c60640d8f37eff
Stefano Linguerri <stefano.linguerri@gmail.com>
2022-05-28 11:08:39 +0200

    setup for demo

--- dev/microservices/kubebrick-factory-ui/app.js
+++ dev/microservices/kubebrick-factory-ui/app.js
@@ -3,16 +3,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-//app.use(express.static(__dirname + 'public')); //Serves resources from public folder
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-
-//process.env.BOOKSURL
-//"http://test.test"
 app.get("/", (req, res) => {
   res.render("index", {
     pagetitle: "Ricerca richiesta", 
