b4cc1e3e67df0f96ce55578be73aedf0015be9e2
Stefano Linguerri <stefano.linguerri@gmail.com>
2022-05-28 12:31:58 +0200

    Cleanup

--- dev/microservices/packaging-notification-service/src/main/java/com/kubebrick/devices/sorting/CamelRouter.java
+++ dev/microservices/packaging-notification-service/src/main/java/com/kubebrick/devices/sorting/CamelRouter.java
@@ -1,24 +1,7 @@
 package com.kubebrick.devices.sorting;
 
-import javax.inject.Inject;
-//import javax.jms.ConnectionFactory;
-
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.builder.FlexibleAggregationStrategy;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
-import org.apache.camel.builder.AggregationStrategies;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.zip.DataFormatException;
 
 public class CamelRouter extends RouteBuilder {
 
